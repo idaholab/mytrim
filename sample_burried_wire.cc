009fc1d3c0396820bd94c63eb7b4c9fccfe6c652
Daniel Schwen <daniel@schwen.de>
2010-11-09 17:12:17 -0600

    fix burried wire geo

--- sample_burried_wire.cc
+++ sample_burried_wire.cc
@@ -14,14 +14,18 @@ sampleBurriedWire::sampleBurriedWire( double x, double y, double z )  : sampleWi
 // look if we are within dr of the wire axis
 materialBase* sampleBurriedWire::lookupMaterial( double* pos ) 
 {
+  // cover layer
+  if( pos[2] < 0.0 && pos[2] >= 250.0 )
+    return material[1];
+
+  // above sample or inside substrate
+  if( pos[2] > w[2] || pos[2] < 250.0  )
+    return 0;
+
+  // in wire layer
   materialBase *ret = sampleWire::lookupMaterial(pos);
   if( ret == 0 )
-  {
-    if( pos[2] > -250.0 )
-      return material[1];
-    else
-      return 0;
-  }
+    return material[1];
   else
     return ret;
 }
