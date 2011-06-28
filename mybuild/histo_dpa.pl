a8897a83321b82f34d9c6802d62c2c5091515b6c
Daniel Schwen <daniel@schwen.de>
2011-06-27 22:53:17 -0500

    output displacements per DPA

--- mybuild/histo_dpa.pl
+++ mybuild/histo_dpa.pl
@@ -5,6 +5,25 @@ $max=2500;
 #$step=0.2;
 $step=1.0;
 
+# density of TiB2
+$rho = 4.52;
+
+# weight per formula unit
+$utib2 = 2*11.0 + 48.0;
+
+# radius in nm
+$r = 1.0;
+
+# volume in cm^3 * 1e21
+$v = 4.0/3.0 * 3.141 * $r**3;
+
+# formula units per bubble
+$a = $rho*$v/$utib2 * 6.022e2; 
+# 1e2 = 1e23*1e-21
+
+# clusters per sample
+$N = 75;
+
 $dp = 0;
 while(<STDIN>)
 {
@@ -15,10 +34,13 @@ while(<STDIN>)
 
 for( $i=0; $i< ($max-$min)/$step; $i++ )
 {
-  $c = $hist[$i] / ($step*$dp);
+  $c = 3.0*$a*$N * $hist[$i] / ($step*$dp);
   $x1 = $i*$step+$min;
   $x2 = $x1 + $step;
   print "$x1\t$c\n";
   print "$x2\t$c\n";
 }
 
+
+
+print 3*$a*$N;
