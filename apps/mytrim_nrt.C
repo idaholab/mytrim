a986278b08ae3828b62ed65f1c9044175652d84b
Sebastian Schunert <sebastian.schunert@inl.gov>
2017-04-27 15:35:33 -0600

    Add power law recoil cross section (#58)

--- apps/mytrim_nrt.C
+++ apps/mytrim_nrt.C
@@ -27,11 +27,6 @@ MaterialBase * _material;
 static const Real math_pi = 3.14159265359;
 static const Real electron_charge = 3.7941; // unit is sqrt(ev * A)
 
-// definitions for computing sigma_ij
-static const std::vector<Real> exponent_m = {1.0 / 3.0, 0.5, 1.0, 0.0};
-static const std::vector<Real> lambda_m = {1.309, 0.327, 0.5, 24.0};
-static const std::vector<Real> epsilon = {15.0, 0.369, 0.0234};
-
 // composition of the problem
 static const std::vector<std::string> names = {"C", "Si"};
 static const std::vector<Real> A = {12.0, 28.0};
@@ -40,7 +35,9 @@ static const std::vector<Real> f = {0.5, 0.5};
 static const std::vector<Real> displacement_threshold = {16.3, 92.6}; // from the paper
 
 // simulation parameters
-static const Real Emax = 1.0e5;
+static const Real Emax = 1.0e3;
+static const Real dE = 1.0;
+static const bool use_power_law = true;
 std::vector<Real> energy_nodes;
 std::vector<std::vector<std::vector<Real>>> nu;
 
@@ -87,7 +84,7 @@ ft(Real t)
 
 
 Real
-recoilCrossSection2(Real projectile_energy, Real target_energy, unsigned int projectile_id, unsigned int target_id)
+recoilCrossSection(Real projectile_energy, Real target_energy, unsigned int projectile_id, unsigned int target_id)
 {
   Real Tm = 4.0 * A[target_id] * A[projectile_id] / sqr(A[target_id] + A[projectile_id]) * projectile_energy;
   if (target_energy > Tm)
@@ -99,46 +96,66 @@ recoilCrossSection2(Real projectile_energy, Real target_energy, unsigned int pro
   Real kappa = projectile_energy * A[projectile_id] / A[target_id] * sqr(a / (2.0 * zt * zp * sqr(electron_charge)));
   Real t = target_energy * kappa;
   Real eta = projectile_energy * A[projectile_id] / A[target_id] * sqr(a / (2.0 * zt * zp * sqr(electron_charge)));
-
   return 0.5 * math_pi * sqr(a) * kappa * ft(t) / pow(t, 3.0 / 2.0);
 }
 
-Real
-recoilCrossSection(Real projectile_energy, Real target_energy, unsigned int projectile_id, unsigned int target_id)
+void
+getPieceWisePowerLaw(Real t, Real & alpha, Real & beta)
 {
-  // check if E->T combination is possible
-  Real gamma = 4.0 * A[target_id] * A[projectile_id] / sqr(A[target_id] + A[projectile_id]);
-  if (target_energy > gamma * projectile_energy)
-    return 0;
-  Real zt = Z[target_id];
-  Real zp = Z[projectile_id];
-  Real a = 0.4683 / sqrt(pow(zt, 2.0 / 3.0) + pow(zp, 2.0 / 3.0));
-  Real eps = projectile_energy / (zt * zp * sqr(electron_charge) / a);
-
-  Real lambda, m;
-  if (eps < epsilon[2])
+  if (t <= 1.0e-5)
   {
-    m = exponent_m[3];
-    lambda = lambda_m[3];
+    alpha = 0;
+    beta = 1;
   }
-  else if (eps > epsilon[2] && eps < epsilon[1])
+  else if (t > 1.0e-5 && t <= 5.0e-3)
   {
-    m = exponent_m[2];
-    lambda = lambda_m[2];
+    alpha = -0.038867;
+    beta = 0.6285;
   }
-  else if (eps > epsilon[1] && eps < epsilon[0])
+  else if (t > 5.0e-3 && t <= 1.0e-1)
   {
-    m = exponent_m[1];
-    lambda = lambda_m[1];
+    alpha = -0.174285;
+    beta = 0.30668;
+  }
+  else if (t > 1.0e-1 && t <= 1.0)
+  {
+    alpha = -0.354466;
+    beta = 0.202539;
+  }
+  else if (t > 1.0    && t <= 10.0)
+  {
+    alpha = -0.50402;
+    beta = 0.2025388;
   }
   else
   {
-    m = exponent_m[0];
-    lambda = lambda_m[0];
+    alpha = -0.641031;
+    beta = 0.3075704;
   }
+}
 
-  Real Cm = 0.5 * math_pi * lambda * sqr(a) * A[projectile_id] / A[target_id] * pow(2.0 * zt * zp * sqr(electron_charge) / a, 2.0 * m);
-  return Cm * pow(projectile_energy, -m) * pow(target_energy, -1.0 - m);
+Real
+recoilCrossSectionPowerLaw(Real projectile_energy, Real target_energy, unsigned int projectile_id, unsigned int target_id)
+{
+  Real Tm = 4.0 * A[target_id] * A[projectile_id] / sqr(A[target_id] + A[projectile_id]) * projectile_energy;
+  if (target_energy > Tm)
+    return 0;
+
+  Real zt = Z[target_id];
+  Real zp = Z[projectile_id];
+  Real At = A[target_id];
+  Real Ap = A[projectile_id];
+  Real lambda = 1.309;
+  Real a0 = 0.4683;
+  Real Eref = 2.0 * sqr(electron_charge) / a0;
+  Real constant = 0.5 * math_pi * lambda * sqr(a0) / Eref;
+  Real Lpt = Ap / At / (sqr(zt * zp) * (pow(zt, 2.0 / 3.0) + pow(zp, 2.0 / 3.0)));
+  Real eps = projectile_energy / Eref;
+  Real tau = target_energy / Eref;
+  Real t =  Lpt * eps * tau;
+  Real alpha, beta;
+  getPieceWisePowerLaw(t, alpha, beta);
+  return constant * Lpt / (pow(zt, 2.0 / 3.0) + pow(zp, 2.0 / 3.0)) * eps * beta * pow(t, alpha - 4.0 / 3.0);
 }
 
 void
@@ -153,7 +170,10 @@ checkData()
   for (auto & e : energies)
   {
     for (auto & t : energies)
-        _console << recoilCrossSection2(e * 1.0e6, t * 1.0e6, 0, 0) << " ";
+      if (use_power_law)
+        _console << recoilCrossSectionPowerLaw(e, t, 0, 0) << " ";
+      else
+        _console << recoilCrossSection(e, t, 0, 0) << " ";
     _console << std::endl;
   }
 }
@@ -189,8 +209,12 @@ nuclearStopping(unsigned int projectile_id, unsigned int target_id, unsigned int
   {
     if (energy_nodes[ll] > Lij * energy_nodes[l])
       return integral;
-    integral += getDeltaE(l) * energy_nodes[ll]
-             * recoilCrossSection2(energy_nodes[l], energy_nodes[ll], projectile_id, target_id);
+    Real xs;
+    if (use_power_law)
+      xs = recoilCrossSectionPowerLaw(energy_nodes[l], energy_nodes[ll], projectile_id, target_id);
+    else
+      xs = recoilCrossSection(energy_nodes[l], energy_nodes[ll], projectile_id, target_id);
+    integral += getDeltaE(l) * energy_nodes[ll] * xs;
   }
   return integral;
 }
@@ -211,8 +235,14 @@ integralI(unsigned int projectile_id, unsigned int target_id, unsigned int l)
     if (energy_nodes[ll] > Lij * energy_nodes[l])
       return integral;
 
+    Real xs;
+    if (use_power_law)
+      xs = recoilCrossSectionPowerLaw(energy_nodes[l], energy_nodes[ll], projectile_id, target_id);
+    else
+      xs = recoilCrossSection(energy_nodes[l], energy_nodes[ll], projectile_id, target_id);
+
     if (energy_nodes[ll] > displacement_threshold[target_id])
-      integral += getDeltaE(ll) * recoilCrossSection2(energy_nodes[l], energy_nodes[ll], projectile_id, target_id);
+      integral += getDeltaE(ll) * xs;
   }
   return integral;
 }
@@ -233,14 +263,18 @@ integralII(unsigned int projectile_id, unsigned int target_id, unsigned int k, u
     if (energy_nodes[ll] > Lik * energy_nodes[l])
       return integral;
 
+    Real xs;
+    if (use_power_law)
+      xs = recoilCrossSectionPowerLaw(energy_nodes[l], energy_nodes[ll], projectile_id, k);
+    else
+      xs = recoilCrossSection(energy_nodes[l], energy_nodes[ll], projectile_id, k);
+
     if (ll == l)
       // in this case we don't know the nu at energy l since we are just computing it.
       // constant approximation consistent with forward Euler tells us to use value at l-1
-      integral += getDeltaE(ll) * nu[k][target_id][ll - 1]
-               * recoilCrossSection2(energy_nodes[l], energy_nodes[ll], projectile_id, k);
+      integral += getDeltaE(ll) * nu[k][target_id][ll - 1] * xs;
     else if (ll != l && energy_nodes[ll] > displacement_threshold[target_id])
-      integral += getDeltaE(ll) * nu[k][target_id][ll]
-               * recoilCrossSection2(energy_nodes[l], energy_nodes[ll], projectile_id, k);
+      integral += getDeltaE(ll) * nu[k][target_id][ll] * xs;
   }
   return integral;
 }
@@ -265,12 +299,17 @@ integralIII(unsigned int projectile_id, unsigned int target_id, unsigned int k, 
     if (Ediff - displacement_threshold[target_id] / Lij < 0.0)
       continue;
 
+    Real xs;
+    if (use_power_law)
+      xs = recoilCrossSectionPowerLaw(energy_nodes[l], energy_nodes[ll], projectile_id, k);
+    else
+      xs = recoilCrossSection(energy_nodes[l], energy_nodes[ll], projectile_id, k);
+
     // find interpolated nu => simply take the value from the left
     unsigned int index = findIndex(Ediff);
     Real interpolated_nu = nu[projectile_id][target_id][index];
 
-    integral += getDeltaE(ll) * interpolated_nu
-             * recoilCrossSection2(energy_nodes[l], energy_nodes[ll], projectile_id, k);
+    integral += getDeltaE(ll) * interpolated_nu * xs;
   }
   return integral;
 }
@@ -287,8 +326,13 @@ integralIV(unsigned int projectile_id, unsigned int target_id, unsigned int k, u
     if (energy_nodes[ll] > Lik * energy_nodes[l])
       return integral;
 
-    integral += getDeltaE(ll) * nu[projectile_id][target_id][l - 1]
-             * recoilCrossSection2(energy_nodes[l], energy_nodes[ll], projectile_id, k);
+      Real xs;
+      if (use_power_law)
+        xs = recoilCrossSectionPowerLaw(energy_nodes[l], energy_nodes[ll], projectile_id, k);
+      else
+        xs = recoilCrossSection(energy_nodes[l], energy_nodes[ll], projectile_id, k);
+
+    integral += getDeltaE(ll) * nu[projectile_id][target_id][l - 1] * xs;
   }
   return integral;
 }
@@ -342,10 +386,9 @@ main()
   _material = new MaterialBase(_simconf, 1.0);
 
   // check out data
-  // checkData();
+  //checkData();
 
   // set up energy_nodes: assume equal spacing
-  Real dE = 10.0;
   Real Ec = 0.0;
   while (Ec < Emax)
   {
