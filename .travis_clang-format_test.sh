68253d972973a6ce6eada4d5e48f43cde5b612e3
Daniel Schwen <daniel@schwen.de>
2019-12-23 16:48:40 -0700

    Use clang-format 9 for travis check

--- .travis_clang-format_test.sh
+++ .travis_clang-format_test.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+CLANG_FORMAT=clang-format
+clang-format-9 --version >/dev/null 2>&1 && CLANG_FORMAT=clang-format-9
+
+# output version
+$CLANG_FORMAT --version
+
 # apply clang-format
-find . -name \*.C -o -name \*.h -exec clang-format -i \{\} \;
+find . -name \*.C -o -name \*.h -exec $CLANG_FORMAT -i \{\} \;
 
 # see if any changes were made
-git diff | tee .diff | grep diff > /dev/null || exit 0
+git diff | tee .diff | grep diff > /dev/null
+if [ $? != 0 ]
+then
+  echo Passed.
+  exit 0
+fi
 
 # changes were made -> branch is not formatted according to rules
 cat .diff
